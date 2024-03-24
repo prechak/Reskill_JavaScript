ecccb9eb47cf9e771af23812c8cd99bebfcd8764
Prechak <benedict.precha@gmail.com>
2024-03-24 19:11:24 +0700

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,32 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
 console.log(40 + 8 + 23 - 10);
+
+console.log("Precha");
+console.log(23);
+
+// How to write variables
+let firstName = "Bob";
+let first = "Precha";
+let firstNamePerson;
+let first_name_person;
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+//  Underscore and dollar sign allow only
+let precha_bob = "PB";
+let $function = 27;
+// Kind of reserved but still canyou
+let name = "Precha";
+
+// Do not do Uppercase first letter
+let Person = "Precha";
+
+// Use for no change value
+let PI = 3.1415;
+
+let myFirstJob = "Coder"; //better than let job 1 = 'programmer'
+let myCurrentJob = "Teacher"; //better than let job2 = 'teacher'
+
+console.log(myFirstJob);
