fa9f563b51c7f4a0ce7fe72da23337f9adaee274
Prechak <benedict.precha@gmail.com>
2024-03-24 21:07:12 +0700

    Taking Decisions if  else Statements_

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -150,6 +150,7 @@ const markHigherBMI = BMIMark > BMIJohn;
 console.log(BMIMark, BMIJohn, markHigherBMI);
 */
 
+/*
 const firstName = "Jonas";
 const job = "teacher";
 const birthYear = 1991;
@@ -173,3 +174,24 @@ line"
 console.log(`String
 multiple
 lines`);
+*/
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving licence 🚘");
+} else {
+  const yearLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
