618191e4f94033d89e5bede6b56d9d7ddb106d22
Prechak <benedict.precha@gmail.com>
2024-03-24 21:35:13 +0700

    Type Conversion and Coercion_

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -198,6 +198,7 @@ if (birthYear <= 2000) {
 console.log(century);
 */
 
+/*
 const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
@@ -213,3 +214,26 @@ if (BMIMark > BMIJohn) {
 } else {
   console.log(`John BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`);
 }
+*/
+
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Jonas"));
+console.log(typeof NaN);
+// NaN is number but not a valid number
+
+console.log(String(23), 23);
+
+// type coercion
+console.log("I am " + 23 + " year old");
+console.log("23" - "10" - 3);
+console.log("23" + "10" + 3);
+console.log("23" * "2");
+console.log("23" / "2");
+
+let n = "1" + 1; // String 11
+n = n - 1; // Strgin 11 - Number 1 = Number 10
+console.log(n);
