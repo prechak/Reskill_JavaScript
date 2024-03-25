e27f6b8d7d1a901b820058c3b08314c226f025ba
Prechak <benedict.precha@gmail.com>
2024-03-25 09:20:25 +0700

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 let hasDriversLincense = false;
 const passTest = true;
 
@@ -9,3 +10,29 @@ if (hasDriversLincense) console.log(`I can drive :D`);
 // strict mode reserved word and have a lot more
 // const interface = `Audio`;
 // const private = 534;
+*/
+
+function logger(myName) {
+  console.log(`My name is ${myName}`);
+}
+
+//  calling / running / invoking fuction
+logger("Precha");
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Jiuce with ${apples} apples ${oranges} oranges.`;
+  return juice;
+}
+// Juice must capture somewhere when return in this case is appleJuice
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// Use it without capture in any variables
+console.log(fruitProcessor(3, 4));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
