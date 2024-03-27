03eb57f899063e7b4f895255de4123e3043b2dea
Prechak <benedict.precha@gmail.com>
2024-03-27 23:00:10 +0700

    Implementing the Game Logic

--- Guess-My-Number/starter/script.js
+++ Guess-My-Number/starter/script.js
@@ -11,6 +11,10 @@ document.querySelector('.score').textContent = 21;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+console.log((document.querySelector('.number').textContent = number));
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -18,5 +22,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number';
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
+  } else if (guess > number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
