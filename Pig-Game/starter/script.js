0e7b07a17c653ea5a012112d34ed05ebd55ee576
Prechak <benedict.precha@gmail.com>
2024-04-02 23:41:02 +0700

    Update scripts

--- Pig-Game/starter/script.js
+++ Pig-Game/starter/script.js
@@ -4,6 +4,7 @@
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
+// Can use query or getEl
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 
