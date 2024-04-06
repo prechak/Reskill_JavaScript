c278283f0561eed1b0fb68ae392d4ece5c434972
Prechak <benedict.precha@gmail.com>
2024-04-06 22:27:37 +0700

    Maps Fundamentals

--- Data-Structures-Operators/starter/script.js
+++ Data-Structures-Operators/starter/script.js
@@ -57,11 +57,48 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+////////////////////////////////////////////////////////
+//***************** Mapp Fundamentals ****************//
+////////////////////////////////////////////////////////
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
 
 ////////////////////////////////////////////////////////
 //************************ Sets **********************//
 ////////////////////////////////////////////////////////
 
+/*
 const orderSet = new Set([
   'Pasta',
   'Pizza',
@@ -97,6 +134,7 @@ console.log(
 
 console.log(new Set('jonasschemedtmann').size);
 console.log(new Set('prechakrasaesup').size);
+*/
 
 ////////////////////////////////////////////////////////
 //* Looping Objects Object Keys, Values, and Entries *//
