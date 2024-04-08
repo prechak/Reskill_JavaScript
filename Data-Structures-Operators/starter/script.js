734787f9295986f2316d809be4190b34c12aef0a
Prechak <benedict.precha@gmail.com>
2024-04-08 23:13:13 +0700

    Coding Challenge #3

--- Data-Structures-Operators/starter/script.js
+++ Data-Structures-Operators/starter/script.js
@@ -706,3 +706,27 @@ const gameEvents = new Map([
   [80, '⚽️ GOAL'],
   [92, '🔶 Yellow card'],
 ]);
+
+// 1.)
+// console.log(gameEvents.values());
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2.)
+gameEvents.delete(64);
+
+// 3.)
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4.
+for (const [min, event] of gameEvents) {
+  const half = min < 45 ? 'First' : 'Second';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
