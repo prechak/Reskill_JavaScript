dec172229eecf26c43c8e93a9874e54e68656675
Prechak <benedict.precha@gmail.com>
2024-04-09 22:44:52 +0700

    Working With Strings #2

--- Data-Structures-Operators/starter/script.js
+++ Data-Structures-Operators/starter/script.js
@@ -103,6 +103,7 @@ console.log(...question.values());
 //*********** Working With Strings - Part 1 **********//
 ////////////////////////////////////////////////////////
 
+/*
 console.log(`----- Working With Strings - Part 1 -----`);
 
 const airline = 'TAP Air Portugal';
@@ -147,6 +148,76 @@ console.log(new String('jonas'));
 console.log(typeof new String('jonas'));
 
 console.log(typeof new String('jonas').slice(1));
+*/
+
+////////////////////////////////////////////////////////
+//*********** Working With Strings - Part 2 **********//
+////////////////////////////////////////////////////////
+
+console.log(`----- Working With Strings - Part 2 -----`);
+
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'jOnAs'; // Jonas
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+  passengerLower[0].toUpperCase() + passengerLower.slice(1);
+
+console.log(passengerCorrect);
+
+// Comparing email
+const email = 'hello@jonas.io';
+const loginEmail = ' Hello@Jonas.Io \n';
+
+const lowerEmail = loginEmail.toLowerCase();
+const trimEmail = lowerEmail.trim();
+console.log(trimEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+
+console.log(email === normalizedEmail);
+
+// replacing
+const priceGB = '288,98฿';
+const priceUS = priceGB.replace('฿', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement = `All passenger come to boarding door 23. Boarding door 23!`;
+
+console.log(announcement.replace('door', 'gate'));
+// console.log(announcement.replaceAll('door', 'gate'));
+
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+console.log(plane.startsWith('Aib'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW AIRbus family');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase(); // lowercase first for everything it sensitive
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard');
+  }
+};
+
+checkBaggage('I have a laptop, some food and a pocket knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
 
 ////////////////////////////////////////////////////////
 //***************** Map Fundamentals ****************//
