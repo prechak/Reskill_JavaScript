d7e8b1c4b309dfe4191d83c7559c172147d3af15
Prechak <benedict.precha@gmail.com>
2024-04-12 19:40:27 +0700

    Coding Challenge #1 - Me

--- Functions/starter/script.js
+++ Functions/starter/script.js
@@ -134,6 +134,7 @@ greet('Hi')('Precha');
 //*         The call and apply Methods              *//
 ///////////////////////////////////////////////////////
 
+/*
 const lufthansa = {
   airline: 'Lufthansa',
   iataCode: 'LH',
@@ -230,3 +231,91 @@ const addTaxRate = function (rate) {
 const addVat2 = addTaxRate(0.23);
 console.log(addVat(100));
 console.log(addVat(23));
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['JavaScript', 'Python', 'Rust', 'C++'],
+  answers: [0, 0, 0, 0],
+
+  registerNewAnswer() {
+    // Display prompt window for user input
+    const input = prompt(
+      `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+    );
+
+    // Convert input to number and check if it's valid
+    const option = Number(input);
+    if (
+      option >= 0 &&
+      option < this.options.length &&
+      Number.isInteger(option)
+    ) {
+      // Update answers array
+      this.answers[option]++;
+      // Display results
+      this.displayResults();
+    } else {
+      alert('Invalid input! Please enter a valid option number.');
+    }
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    } else {
+      console.log('Invalid display type. Please specify "array" or "string".');
+    }
+  },
+};
+
+// Button click event listener
+document.querySelector('.poll').addEventListener('click', function () {
+  poll.registerNewAnswer();
+});
+
+// // Bonus Test Data 1
+// const testData1 = [5, 2, 3];
+// poll.displayResults.call({ answers: testData1 }, 'array'); // Display results as array
+// poll.displayResults.call({ answers: testData1 }, 'string'); // Display results as string
+
+// // Bonus Test Data 2
+// const testData2 = [1, 5, 3, 9, 6, 1];
+// poll.displayResults.call({ answers: testData2 }, 'array'); // Display results as array
+// poll.displayResults.call({ answers: testData2 }, 'string'); // Display results as string
