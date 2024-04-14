edf6e556d0d25170eb175e936179fac4fe0eea18
Prechak <benedict.precha@gmail.com>
2024-04-15 02:42:34 +0700

    Closures #2

--- Functions/starter/script.js
+++ Functions/starter/script.js
@@ -340,6 +340,7 @@ console.log(notPrivate);
 //*                   Closures                      *//
 ///////////////////////////////////////////////////////
 
+/*
 const secureBooking = function () {
   let passengerCount = 0;
 
@@ -356,3 +357,45 @@ booker();
 booker();
 
 console.dir(booker);
+*/
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passenger`);
+    console.log(`There are 3 groups, each with ${perGroup} passenger`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} second`);
+};
+
+// const perGroup = 1000;
+boardPassengers(180, 3);
