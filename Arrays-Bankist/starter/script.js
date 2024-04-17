07f526c4017c3ac5c242fad5c2e64858eb9fffc3
Prechak <benedict.precha@gmail.com>
2024-04-17 21:44:52 +0700

    Creating DOM Elements

--- Arrays-Bankist/starter/script.js
+++ Arrays-Bankist/starter/script.js
@@ -61,6 +61,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovement = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+
+    <div class="movements__value">${mov}</div>
+  </div>
+  `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovement(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -160,7 +179,7 @@ movements.forEach(function (mov, i, arr) {
 */
 
 // --------------- forEach Map and Set ---------------
-
+/*
 // Map
 currencies.forEach(function (value, key, map) {
   console.log(`${key}: ${value}`);
@@ -172,3 +191,4 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
+*/
