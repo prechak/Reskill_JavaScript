61f889ec818747ec38121e50879d1586fb1ec633
Prechak <benedict.precha@gmail.com>
2024-04-19 18:08:44 +0700

    The Magic of Chaining Methods

--- Arrays-Bankist/starter/script.js
+++ Arrays-Bankist/starter/script.js
@@ -80,6 +80,36 @@ const displayMovement = function (movements) {
 };
 displayMovement(account1.movements);
 
+const calcPrintBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calcPrintBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((inter, i, arr) => {
+      console.log(arr);
+      return inter >= 1;
+    })
+    .reduce((acc, inter) => acc + inter, 0);
+  labelSumInterest.textContent = `${interest}`;
+};
+calcDisplaySummary(account1.movements);
+
 // const user = 'Steven Thomas Williams'; // stw
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -94,16 +124,11 @@ const createUsernames = function (accs) {
 createUsernames(accounts);
 // console.log(accounts);
 
-const calcPrintBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
-};
-
-calcPrintBalance(account1.movements);
-
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+const eurToUsd = 1.1;
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -371,6 +396,7 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
+/*
 const calcAverageHumanAge = function (dogAges) {
   const humanAge = dogAges.map(dogAge =>
     dogAge <= 2 ? dogAge * 2 : 16 + dogAge * 4
@@ -392,3 +418,15 @@ const calcAverageHumanAge = function (dogAges) {
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 console.log(`----------`);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+*/
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  // .map(mov => mov * eurToUsd)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
