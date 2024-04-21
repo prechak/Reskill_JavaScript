d0dea61829a412144a01ec8dec68b4cfe1043146
Prechak <benedict.precha@gmail.com>
2024-04-21 15:52:06 +0700

    More Ways of Creating and Filling Arrays

--- Arrays-Bankist/starter/script.js
+++ Arrays-Bankist/starter/script.js
@@ -661,6 +661,7 @@ const overallBalance3 = accounts
 console.log(overallBalance3);
 */
 
+/*
 //////////////////////////////////////
 // Sorting Arrays
 
@@ -678,12 +679,10 @@ console.log(movements);
 // return > 0, B, A (switch order)
 
 // Ascending
-/*
-movements.sort((a, b) => {
-  if (a > b) return 1;
-  if (a < b) return -1;
-});
-*/
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
 // Instead we can using this
 movements.sort((a, b) => a - b);
 console.log(`----- Ascending -----`);
@@ -699,3 +698,49 @@ console.log(`----- Descending -----`);
 movements.sort((a, b) => b - a);
 
 console.log(movements);
+*/
+
+//////////////////////////////////////
+// More Ways of Creating and Filling Arrays
+
+// Basicly
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5)); // -> Not Work!
+
+x.fill(1, 3, 5);
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+const roll = Array.from(
+  { length: 100 },
+  () => Math.trunc(Math.random() * 6) + 1
+);
+console.log(roll);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+
+  console.log(movementsUI);
+
+  // Other way to convert querySelector to array
+  const movementUI2 = [...document.querySelectorAll('.movements__value')];
+  console.log(movementUI2);
+});
