2084c569c36b5ffe5c9a9722827bf5931177c5f5
Prechak <benedict.precha@gmail.com>
2024-04-24 15:55:20 +0700

    The Remainder Operator

--- Numbers-Dates-Timers-Bankist/starter/script.js
+++ Numbers-Dates-Timers-Bankist/starter/script.js
@@ -293,6 +293,7 @@ console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
 */
 
+/*
 /////////////////////////////////////////////////
 // Math and Rounding
 
@@ -338,3 +339,32 @@ console.log(Math.floor(-23.3));
 console.log((2.7).toFixed(0)); // Return string
 console.log((2.7).toFixed(3));
 console.log(+(2.345).toFixed(2));
+*/
+
+/////////////////////////////////////////////////
+// The Remainder Operator
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    // 0,2,4,6
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    // 0,3,6,9
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
+
+// Nth
